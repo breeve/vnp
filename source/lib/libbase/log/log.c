2568cd0f5dd99dd778325358e887c738356b4487
breevewindys <1205034601@qq.com>
2019-12-29 21:36:23 +0800

    log库基本代码

--- source/lib/libbase/log/log.c
+++ source/lib/libbase/log/log.c
@@ -1,21 +1,51 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 #include "base/log.h"
 
-static char * g_appname = "default";
+#define LOG_BUFF_MAX (1024)
+
+static char * s_appname = "default";
+static char s_log_buff[LOG_BUFF_MAX] = {'\0'};
+static char s_log_format[LOG_BUFF_MAX] = {'\0'};
+
+char * level_2_str(log_level level)
+{
+    switch(level) {
+        case INFO_LEVEL:
+            return "INFO";
+        case ERROR_LEVEL:
+            return "ERROR";
+        default:
+            return "unknow";
+    }
+
+    return "unknow";
+}
+
+void log_out(const char *mode,
+    const char * file, int file_len,
+    const char * func, int func_len,
+    long line, log_level level, 
+    const char * format, ...)
+{
+    va_list args;
+    memset(s_log_buff, 0, sizeof(s_log_buff));
+    memset(s_log_format, 0, sizeof(s_log_format));
+
+    va_start(args, format);
+
+    snprintf(s_log_format, sizeof(s_log_format), "[%s] [%s] %s", mode, level_2_str(level), format);
+
+    vsnprintf(s_log_buff, LOG_BUFF_MAX-1, s_log_format, args);
+
+    printf("%s", s_log_buff);
+
+    va_end(args);
+}
 
 void log_app_init(char * appname)
 {
-	g_appname = strdup(appname);
-	printf("app:%s log init success\n", g_appname);
-}
-
-void log_info(char *mode, char *msg)
-{
-	printf("[%s] INFO %s\n", mode, msg);
-}
-
-void log_error(char *mode, char *msg)
-{
-	printf("[%s] ERROR %s\n", mode, msg);
+    s_appname = strdup(appname);
+    log_info("log", "%s log init success\n", s_appname);
 }
